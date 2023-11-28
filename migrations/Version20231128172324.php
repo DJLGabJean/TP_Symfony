806ab01388ff5929b4a8aac9b43fdafd4aa40775
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-11-28 19:17:17 +0100

    test: adding missing migration

---
+++ migrations/Version20231128172324.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231128172324 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE brand (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE clothing (id INT AUTO_INCREMENT NOT NULL, category_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_139C38B112469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE clothing_brand (clothing_id INT NOT NULL, brand_id INT NOT NULL, INDEX IDX_3F5B65664CFB3290 (clothing_id), INDEX IDX_3F5B656644F5D008 (brand_id), PRIMARY KEY(clothing_id, brand_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE clothing_size (clothing_id INT NOT NULL, size_id INT NOT NULL, INDEX IDX_273BA59D4CFB3290 (clothing_id), INDEX IDX_273BA59D498DA827 (size_id), PRIMARY KEY(clothing_id, size_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE shoes (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE size (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sweat (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tee_shirt (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE clothing ADD CONSTRAINT FK_139C38B112469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('ALTER TABLE clothing_brand ADD CONSTRAINT FK_3F5B65664CFB3290 FOREIGN KEY (clothing_id) REFERENCES clothing (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE clothing_brand ADD CONSTRAINT FK_3F5B656644F5D008 FOREIGN KEY (brand_id) REFERENCES brand (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE clothing_size ADD CONSTRAINT FK_273BA59D4CFB3290 FOREIGN KEY (clothing_id) REFERENCES clothing (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE clothing_size ADD CONSTRAINT FK_273BA59D498DA827 FOREIGN KEY (size_id) REFERENCES size (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE clothing DROP FOREIGN KEY FK_139C38B112469DE2');
+        $this->addSql('ALTER TABLE clothing_brand DROP FOREIGN KEY FK_3F5B65664CFB3290');
+        $this->addSql('ALTER TABLE clothing_brand DROP FOREIGN KEY FK_3F5B656644F5D008');
+        $this->addSql('ALTER TABLE clothing_size DROP FOREIGN KEY FK_273BA59D4CFB3290');
+        $this->addSql('ALTER TABLE clothing_size DROP FOREIGN KEY FK_273BA59D498DA827');
+        $this->addSql('DROP TABLE brand');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE clothing');
+        $this->addSql('DROP TABLE clothing_brand');
+        $this->addSql('DROP TABLE clothing_size');
+        $this->addSql('DROP TABLE shoes');
+        $this->addSql('DROP TABLE size');
+        $this->addSql('DROP TABLE sweat');
+        $this->addSql('DROP TABLE tee_shirt');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
