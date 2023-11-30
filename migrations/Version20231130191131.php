a2d00a0fdf5460f5487f7cd64a0cd51198d18668
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-11-30 21:19:33 +0100

    fix: Adding a new migration

--- migrations/Version20231130191131.php
+++ migrations/Version20231130191131.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231129030214 extends AbstractMigration
+final class Version20231130191131 extends AbstractMigration
 {
     public function getDescription(): string
     {
