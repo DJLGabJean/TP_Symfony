08d11774372426a982ce8ee4f9ac60ba4df2f177
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-12-01 01:50:50 +0100

    feat: The page for adding clothing is functional + updating twig + adding remove.css

--- src/Form/ClothingType.php
+++ src/Form/ClothingType.php
@@ -11,19 +11,25 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Count;
 
 class ClothingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, options:['label' => 'Clothing Name'])
+            ->add('name', TextType::class, [
+                'label' => 'Clothing Name', 
+            ])
             ->add('categories', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
                 'multiple' => true, 
                 'expanded' => true, 
-                'by_reference' => false, 
+                'by_reference' => false,
+                'constraints' => [
+                    new Count(['min' => 1, 'minMessage' => 'Please choose at least one category.'])
+                ]
             ])
             ->add('brand', EntityType::class, [
                 'class' => Brand::class,
@@ -34,10 +40,11 @@ class ClothingType extends AbstractType
                 'choice_label' => 'name',
                 'multiple' => true, 
                 'expanded' => true, 
-                'by_reference' => false
+                'by_reference' => false,
+                'constraints' => [
+                    new Count(['min' => 1, 'minMessage' => 'Please choose at least one size.'])
+                ]
             ]);
-
-            
     }
 
     public function configureOptions(OptionsResolver $resolver): void
